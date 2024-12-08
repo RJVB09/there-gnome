00133bc30499c19faf84d10cb8f7684362b0d5a3
RJVB09 <49564898+RJVB09@users.noreply.github.com>
2024-12-08 23:07:20 +0100

    PYSINEWAVE YIPPIE

--- tests/serial_read.py
+++ tests/serial_read.py
@@ -1,21 +1,30 @@
-import winsound
-
 import matplotlib.pyplot as plt
 import serial as ser
 
-arduino = ser.Serial(port="COM9", baudrate=9600, timeout=0.1)
+from pysinewave import SineWave
 
-data_list = []
+arduino = ser.Serial(port="COM4", baudrate=9600, timeout=0.1)
+
+sinewave = SineWave(pitch = 12, pitch_per_second = 1000)
+
+playing = False
+
+sinewave.play()
 
 while 1:
 
     data = arduino.readline()
 
     decoded_data = data.decode("utf-8")
+    distance_mm = int(decoded_data)
     #data_list.append(int(decoded_data))
-    #print(int(decoded_data))
+
+    #sinewave.set_pitch(int(distance_mm/20 - 1))
+    sinewave.set_pitch(18-int(distance_mm/20 - 1))
+    #sinewave.set_frequency(distance_mm + 200)
+
+    print(distance_mm)
     # print(int(800.0/1500.0 * int(decoded_data) + 200.0))
-    winsound.Beep(int(700.0/1500.0 * int(decoded_data) + 200.0), 10)
 
 
 # print(rm.list_resources())
---
+++ tests/test.py
@@ -0,0 +1,20 @@
+import time
+
+from pysinewave import SineWave
+
+# Create a sine wave, with a starting pitch of 12, and a pitch change speed of 10/second.
+sinewave = SineWave(pitch = 12, pitch_per_second = 1000)
+
+# Turn the sine wave on.
+sinewave.play()
+
+# Sleep for 2 seconds, as the sinewave keeps playing.
+time.sleep(2)
+
+# Set the goal pitch to -5.
+sinewave.set_pitch(0)
+
+# Sleep for 3 seconds, as the sinewave smoothly slides its pitch down from 12 to -5, and stays there.
+time.sleep(3)
+
+sinewave.stop()
